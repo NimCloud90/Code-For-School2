37748961fa8456e757e4d0b579a30443ff52612b
NimCloud90 <shayleefarris@gmail.com>
2025-01-16 20:05:37 -0600

    did question four

--- js lesson/script.js
+++ js lesson/script.js
@@ -1,8 +1,13 @@
 let nameTwo = `Shay`;
 console.log(nameTwo);
 
+let nameThree = `Farris`;
+console.log(nameThree);
+
 let color = `purple`;
 console.log(color);
+
+console.log(`Welcome, ${nameTwo}, who's favorite color is ${color}!`);
 
 let totalPrice = 18.90;
 const Price = 15.12;
