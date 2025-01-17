5965fa0fcf8fab78ad11ef44c0c5d4c69b010b44
NimCloud90 <shayleefarris@gmail.com>
2025-01-16 19:55:46 -0600

    i tried doing the second question, but i know i did it wrong so i need to fix it

--- js lesson/script.js
+++ js lesson/script.js
@@ -4,6 +4,11 @@ console.log(nameTwo);
 let color = `purple`;
 console.log(color);
 
-let totalPrice = 15.12
-console.log(totalPrice);
+let totalPrice = 18.90;
+const Price = 15.12;
+const tax = 7;
+const total = totalPrice + Price * 7;
+console.log(`Total price: $${total.toFixed(2)}`)
+
+
 
