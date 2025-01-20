97fb3dc9d7156606c2f2f47d2f28bbb71133eb76
NimCloud90 <shayleefarris@gmail.com>
2025-01-20 17:59:23 -0600

    boolean things

--- js lesson/script.js
+++ js lesson/script.js
@@ -1,4 +1,15 @@
 console.log(true);
 console.log(false);
 
+//comparison operators
+//< > <= >= === = !=
 
+console.log(10>5);
+console.log(5 === "5");
+console.log(5 != 3);
+
+//Truthy and Falsy
+
+console.log(Boolean(-4));
+console.log(Boolean);
+console.log(Boolean("."));
