a3bc24b8bed7fd4a5425bf281b4e325eaafc21e6
NimCloud90 <shayleefarris@gmail.com>
2025-01-20 18:17:24 -0600

    the same as last time, but Dragon Quest themed

--- js lesson/script.js
+++ js lesson/script.js
@@ -1,8 +1,7 @@
-let isWeekend = false;
-let isHoliday = false;
+let Metalslime = false;
 
-if (isWeekend || isHoliday) {
-    console.log('You can relax today');
+if (Metalslime) {
+    console.log('You have slain the elusive Metal Slime, good job Yggdrasil is pleased!');
 } else {
-    console.log('It is a workday');
+    console.log('It has once again slipped from your grasp, as it has taunts you with its beady and empty eyes before slithering away');
 }
