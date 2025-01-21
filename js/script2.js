5a3d1e188eff6c3c34967e446dddb3cfaca546b1
NimCloud90 <shayleefarris@gmail.com>
2025-01-20 20:35:37 -0600

    did problem 4

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,26 @@
-console.log(Boolean(0));
-console.log(Boolean(""));
-console.log(Boolean('Hello'));
-console.log(Boolean(42));
-console.log(Boolean(null));
+//console.log(Boolean(0));
+//console.log(Boolean(""));
+//console.log(Boolean('Hello'));
+//console.log(Boolean(42));
+//console.log(Boolean(null));
+
+//let age = 15;
+
+//if (age >= 18) {
+//    console.log('You are eligible to vote');
+//} else {
+ //   console.log('You are not eligible to vote');
+//}
+
+const username = 'admin';
+const password = 'password123';
+
+if (username === 'admin') {
+    if (password === 'password123') {
+        console.log('Access Granted');
+    } else {
+        console.log('Incorrect password');
+    }
+} else {
+    console.log('Unknown user');
+}
