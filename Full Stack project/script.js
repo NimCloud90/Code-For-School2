849f0d3d89a5aab0fae471249ef3aff662758eb1
NimCloud90 <shayleefarris@gmail.com>
2025-01-23 16:59:01 -0600

    nvm with that idea i guess i don't know how to do things rn

--- Full Stack project/script.js
+++ Full Stack project/script.js
@@ -1,7 +1,3 @@
-let Metalslime = false;
-
-if (Metalslime) {
-    console.log('You have slain the elusive Metal Slime, good job Yggdrasil is pleased!');
-} else {
-    console.log('It has once again slipped from your grasp, as it has taunts you with its beady and empty eyes before slithering away');
+button onclick {
+    console.log(Math.random())
 }
