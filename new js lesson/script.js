feb8ae379e813c2db36819c644d155b1f4c9518d
NimCloud90 <shayleefarris@gmail.com>
2025-01-24 17:46:03 -0600

    button and stuff works

--- new js lesson/script.js
+++ new js lesson/script.js
@@ -1,10 +1,18 @@
 const car = {
-    make: 'Toyoda',
-    model: 'Honda',
+    make: 'Honda',
+    model: 'Accord',
     year: 1990,
-    getInfo: function(getInfo)
-    {console.log(car.make,
-        car.model,
-        car.year,
-    );}
+    getInfo: function() {
+        console.log(`${car.make},
+            ${car.model},
+            ${car.year}`
+        );
+    }
 };
+
+car.getInfo();
+
+
+const element = document.getElementById('carrrrss');
+element.textContent = 'console isnt working?'
+console.log(element);  
