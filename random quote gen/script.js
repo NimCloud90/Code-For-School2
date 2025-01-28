098581a37404cc7e6ab9bd1602d1fbe43c9f8c5e
NimCloud90 <shayleefarris@gmail.com>
2025-01-27 18:32:58 -0600

    the quote generator is working :D

--- random quote gen/script.js
+++ random quote gen/script.js
@@ -1,6 +1,15 @@
 const quotes = [
-
-
-
+    "Who's blood is that? - El",
+    "Floors aren't supposed to move, they're supposed to *floor*. - Freya",
+    "The phoenix enters. It is wearing a tophat and it is *very angry*. - El",
+    "Can we disregard the reality warping rats for a moment? - Freya",
+    "Caroline is teaching Ezra how to do drugs. - Shay",
     
 ]
+
+document.getElementById('button').addEventListener('click', () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+
+    document.getElementById('quote-display').innerText = `"${quotes[randomIndex]}"`;
+}
+);
