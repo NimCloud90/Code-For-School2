b323310b7762b75034d714a5bd2e78dbb65da524
NimCloud90 <shayleefarris@gmail.com>
2025-01-28 19:25:18 -0600

    more quotes for the quote god

--- random quote gen/script.js
+++ random quote gen/script.js
@@ -8,8 +8,20 @@ const quotes = [
     "Oh, you don't know about the Bagel Dimension. - Jackson",
     "I kick him anyway it's still gonna hurt! - Freya",
     "The other pumpkins get angry at you. -Freya",
-    "heyyyyy mam did you know i have a plus two to boat handling.  -el to the living boat",
-    
+    "heyyyyy mam did you know i have a plus two to boat handling. - el to the living boat",
+    "*whispers* He's fucking my dad. - Freya as Basil.",
+    "tactical hallucinogenic spaghetti - Freya",
+    "Ronald Raegan, more like Ronald Rail me. - I assume it's Jackson",
+    "PLease don't use WebP files. - Jackson literally sobbing",
+    "Rio now has the will to live - Sky",
+    "DON'T BE RACIST! - El as a talking dog",
+    "Our mailbox number is Marty. -Shay",
+    "'Stop! This isn't you! Blockout the haters!' That's what this is giving. -Sky",
+    "So, whoever said William Afton is morally questionable: no. -Shay",
+    "If you want to murder a child, do it yourself. -Jackson",
+    "i was sufficating in meat. - el as robin",
+    "'My wife is a beautiful woman!' 'And a bitch.' -Random Man, Sky as a rat",
+    "YOU SAID YOU'D BEHAVE!!!! - Me and El",
 ]
 
 document.getElementById('button').addEventListener('click', () => {
