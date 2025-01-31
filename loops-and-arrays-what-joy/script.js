5728af720cd579c287255ad5c63b3e632062db00
NimCloud90 <shayleefarris@gmail.com>
2025-01-30 19:56:26 -0600

    i had an idea, and it didn't really work but i think it made since to try

---
+++ loops-and-arrays-what-joy/script.js
@@ -0,0 +1,20 @@
+const todoList = ['Clean room', 'Grocery Shopping', 'Eat something'];
+todoList.push('Quit The Dollar General');
+console.log(todoList);
+todoList.shift();
+console.log(todoList);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const evens = [];
+
+// for (const number of numbers) {
+//     if (number % 2 === 0) {
+//         evens.push(number);
+//     }
+// }
+
+// console.log(evens);
+
+const numbers = [1, 2, 3, 4];
+numbers[0, 1, 2, 3] = 2, 4, 1, 3;
+console.log(numbers);
